c101a5b459303ccf4a22f0b65a647eb34e5674f1
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-10-09 01:05:36 +0430

    Handle the add book

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,6 +1,7 @@
 import {useState} from 'redux';
 import { useDispatch } from 'react-redux';
-import {addBook, removeBook} from './redux/books/books';
+import {addBook} from './redux/books/books';
+import { v4 as uuidv4 } from "uuid"
 
 const AddBook = () => {
   const initialState = {title:'',author:''}
@@ -16,13 +17,23 @@ const AddBook = () => {
     } 
   }
 
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+    const newBook = {
+        id: uuidv4(),
+        title:book.title,
+        author:book.author,
+    }
+    dispatch(addBook(newBook));
+}
+
   return (
-    <form>
+    <form onSubmit={submitBookToStore}>
       Book Name:
       <input type='text' value={book.title} name='title' onChange={onChange} />
       Author:
       <input type='text' value={book.author} name='author' onChange={onChange} />
-      <button type="submit">Add</button>
+      <button type="submit">Add Book</button>
     </form>
   );
 };
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,18 @@
 /* eslint react/destructuring-assignment: 0 */
 /* eslint react/prop-types: 0 */
-const Book = (props) => (
-  <div>
-    {props.book.name}
-    {props.book.author}
-    <button type="submit">Remove</button>
-  </div>
-);
+import { useDispatch } from 'react-redux';
+import {addBook} from './redux/books/books';
+
+const Book = (props) => {
+  const removeBookFromStore = (e) => {
+    
+  }
+  return(
+    <div>
+      {props.book.name}
+      {props.book.author}
+      <button id={props.book.id} onClick={removeBookFromStore}>Remove</button>
+    </div>
+  );
+};
 export default Book;
